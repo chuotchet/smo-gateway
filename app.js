721a9d84262629cfb72f5488534b7670174dfc14
chuotchet <chuotchet94@gmail.com>
2016-10-14 00:16:18 +0700

    ddf

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressLayouts = require('express-ejs-layouts');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +15,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(expressLayouts);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,7 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,85 @@
 var express = require('express');
+var jsonfile = require('jsonfile');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var userfile = './config/user.json';
 var router = express.Router();
+var checkPassword = function(password, callback){
+  jsonfile.readFile(userfile, function(err, user){
+    callback(user.password==password);
+  });
+}
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+    console.log('redirect');
+		res.redirect('/login');
+	}
+}
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    if(username!='admin'){
+      console.log('usernamesss');
+      return done(null, false);
+    }
+    checkPassword(password, function(isMatch){
+      if(isMatch) return done(null, username);
+      else return done(null, false);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+/* GET home page. */
+router.get('/', ensureAuthenticated, function(req, res) {
+  res.render('index');
+});
+
+router.get('/login', function(res, res){
+  res.render('login')
+});
+
+router.post('/login',
+  passport.authenticate('local')
+  ,function(req, res){
+  res.redirect('/');
+});
+
+router.get('/changepass', ensureAuthenticated, function(req,res){
+  res.render('changepassword');
+});
+
+router.post('/changepass', ensureAuthenticated, function(req,res){
+  var oldpass = req.body.oldpass;
+  var newpass = req.body.newpass;
+  jsonfile.readFile(userfile, function(err, user){
+    if(user.password==oldpass){
+      user.password = newpass;
+      jsonfile.writeFile(userfile, user, function(err){
+        console.error(err);
+        res.send('Change password successfully!');
+      });
+    }
+    else {
+      res.send('Password is incorrect!');
+    }
+  });
+});
+
+router.get('/qrcode', ensureAuthenticated, function(req,res){
+  res.locals.token = {
+    MAC: "asdasdasdada",
+    token: "ahihihihdfdddddddddddddddddddddddddddddddddd"
+  }
+  res.render('qrcode');
+});
 module.exports = router;
